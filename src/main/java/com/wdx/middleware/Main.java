e6f70439a49beb1db1b0b5e6f8859e3e16dec67d
王东晓 <wb-wdx330446@antfin.com>
2018-03-15 16:24:46 +0800

    删除多余代码

--- src/main/java/com/wdx/middleware/Main.java
+++ src/main/java/com/wdx/middleware/Main.java
@@ -1,6 +1,5 @@
 package com.wdx.middleware;
 
-import com.wdx.middleware.aop.ProxyAopHandler;
 import com.wdx.middleware.service.DemoService;
 import com.wdx.middleware.service.impl.DemoServiceImpl;
 
@@ -14,21 +13,21 @@ public class Main {
         long start = System.currentTimeMillis();
         List<Thread> workers = new ArrayList<Thread>();
         for(int i = 0 ; i<10;i++) {
-            Thread t = new Thread(new Runnable() {
-                public void run() {
-                    DemoService demoService = new DemoServiceImpl();
-                    DemoService service = (DemoService) Proxy.newProxyInstance(
-                            demoService.getClass().getClassLoader(),
-                            demoService.getClass().getInterfaces(),
-                            new ProxyAopHandler(demoService));
-                    String result = service.say("hello world");
-
-                    System.out.println(result);
-                }
-            });
-
-            t.start();
-            workers.add(t);
+//            Thread t = new Thread(new Runnable() {
+//                public void run() {
+//                    DemoService demoService = new DemoServiceImpl();
+//                    DemoService service = (DemoService) Proxy.newProxyInstance(
+//                            demoService.getClass().getClassLoader(),
+//                            demoService.getClass().getInterfaces(),
+//                            new ProxyAopHandler(demoService));
+//                    String result = service.say("hello world");
+//
+//                    System.out.println(result);
+//                }
+//            });
+//
+//            t.start();
+//            workers.add(t);
         }
         for(int i = 0; i < workers.size(); i++) {
             workers.get(i).join();
